09559019fe22e30d9f2c973ff23b9a2bcefeeb90
Kevin Patel <42532908+KevinPatel04@users.noreply.github.com>
2020-02-15 14:39:15 +0530

    Update DLIB_FRONTAL_FACE_DETECTION.py

--- DLIB_FRONTAL_FACE_DETECTION.py
+++ DLIB_FRONTAL_FACE_DETECTION.py
@@ -4,32 +4,33 @@ import time
 import cv2
 
 face_detector = dlib.get_frontal_face_detector()
-face_count = 0
 
 # specify the image path [ .jpg, .png, .jpeg]
 path = PATH_OF_IMAGE
 
 # function to detect faces
 def DetectFace(data_path):
+    # initialize face count to 0
+    face_count = 0
+    
     # read the image file
     img = cv2.imread(data_path)
     faces = face_detector(img, 1)
-    imgframe = img
     for face in faces:
         x = face.left()
         y = face.top()
         w = face.right() - x
         h = face.bottom() - y
         # draw box over face
-        cv2.rectangle(imgframe, (x,y), (x+w,y+h), (0,0,255), 2)
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,255), 2)
         # count the number of face detected
         face_count = face_count+1
 
-    if len(result_list)>0:
+    if len(faces)>0:
         print("{} Face Detected".format(face_count))
     else:
         print("No Face Detected!!")
-    cv2.imwrite('mtcnn_result_img.jpg'.format(count),imgframe)
+    cv2.imwrite('dlib_frontal_face_detector_result.jpg',img)
 start = time.time()
 DetectFace(path)
 end = time.time()
