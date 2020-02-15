5eeedae0d72e23e1b8476a241c8e3ae97873fd83
Kevin Patel <42532908+KevinPatel04@users.noreply.github.com>
2020-02-15 14:34:59 +0530

    Update HAAR_FACE_DETECTION.py

--- HAAR_FACE_DETECTION.py
+++ HAAR_FACE_DETECTION.py
@@ -5,17 +5,18 @@ import os
 # Load the cascade
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-face_count = 0
-
 # specify the image path [ .jpg, .png, .jpeg]
 path = PATH_OF_IMAGE
 
 # function to detect faces
 def DetectFace(data_path):
+    # initialize face count to 0
+    face_count = 0
+    
     # read the image file
     data = cv2.imread(data_path)
     # Convert to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(data, cv2.COLOR_BGR2GRAY)
     # Detect the faces
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
     # Draw the rectangle around each face
@@ -28,7 +29,7 @@ def DetectFace(data_path):
         print("{} Face Detected".format(face_count))
     else:
         print("No Face Detected!!")
-    cv2.imwrite('haar_result_img.jpg'.format(count),imgframe)
+    cv2.imwrite('haar_result_img.jpg',data)
 start = time.time()
 DetectFace(path)
 end = time.time()
